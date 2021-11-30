0a181768b7e61ae70d58a673cf11767dae6e13c9
Chris Bonifacio <christopher.bonifacio@gmail.com>
2021-11-30 11:31:40 -0500

    add amplify deps

--- next/starter/pages/_app.js
+++ next/starter/pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Amplify, withSSRContext } from "aws-amplify";
+import { AmplifyProvider } from "@aws-amplify/ui-react";
+import awsconfig from "../src/aws-exports";
+Amplify.configure({ ...awsconfig, ssr: true });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AmplifyProvider>
+      <Component {...pageProps} />
+    </AmplifyProvider>
+  );
 }
 
-export default MyApp
+MyApp.getInitialProps = async ({ ctx }) => {
+  let user = null;
+
+  try {
+    const { Auth } = withSSRContext(ctx);
+
+    user = await Auth.currentAuthenticatedUser();
+
+    console.log({ user });
+
+    return {
+      pageProps: {
+        user,
+      },
+    };
+  } catch (error) {
+    return {
+      pageProps: {
+        error,
+      },
+    };
+  }
+};
+
+export default MyApp;
