1d66fb1ffea39eb2d1111dedfe8f9c4dbe834e33
Chris Bonifacio <christopher.bonifacio@gmail.com>
2021-12-14 13:51:55 -0500

    create repro for issue #9352

--- issues/datastore/issue9352/src/App.js
+++ issues/datastore/issue9352/src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Amplify, DataStore } from "aws-amplify";
+import { Todo } from "./models";
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = async (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await DataStore.save(new Todo({ title, completed: false }));
+
+      setTitle("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const completeTodo = async (id) => {
+    try {
+      const original = await DataStore.query(Todo, id);
+      await DataStore.save(
+        Todo.copyOf(original, (updated) => {
+          updated.completed = !original.completed;
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeTodo = async (id) => {
+    try {
+      const todoToDelete = await DataStore.query(Todo, id);
+      await DataStore.delete(todoToDelete);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const subTodos = DataStore.observeQuery(Todo).subscribe({
+      next: ({ items, isSynced }) => {
+        isSynced && setTodos(items);
+      },
+    });
+
+    return () => subTodos.unsubscribe();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <h1>DataStore observeQuery</h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={handleChange}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <pre>{JSON.stringify(todo, null, 2)}</pre>
+            <button onClick={() => completeTodo(todo.id)}>
+              Toggle Complete
+            </button>
+            <button onClick={() => removeTodo(todo.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ issues/datastore/issue9352/src/models/schema.js
@@ -0,0 +1,57 @@
+export const schema = {
+    "models": {
+        "Todo": {
+            "name": "Todo",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "title": {
+                    "name": "title",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "completed": {
+                    "name": "completed",
+                    "isArray": false,
+                    "type": "Boolean",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Todos",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "e0e85c3ecb2ed59c091a5836c594c0f2"
+};
