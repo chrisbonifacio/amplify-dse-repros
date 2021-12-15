62ec009a9f1a63160ef4d12468d7e02cdea92f5a
Chris Bonifacio <christopher.bonifacio@gmail.com>
2021-12-15 16:51:26 -0500

    commit before eject

---
+++ issues/predictions/issue9351/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,24 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "issue9351e54ab3df": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "transcriptionce3fad64": {
+            "region": "string",
+            "language": "string"
+        },
+        "speechGenerator5dfe9e38": {
+            "region": "string",
+            "language": "string",
+            "voice": "string"
+        }
+    }
+}
