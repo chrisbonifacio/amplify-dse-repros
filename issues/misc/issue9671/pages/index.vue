ea1383dd18b0e9abfeeec2d460713ddd6ef6bc6b
Chris Bonifacio <christopher.bonifacio@gmail.com>
2022-03-10 11:37:43 -0500

    update repros

---
+++ issues/misc/issue9671/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+import {Amplify, API} from 'aws-amplify'
+import {listTodos} from '../src/graphql/queries'
+import awsconfig from '../src/aws-exports'
+
+Amplify.configure(awsconfig)
+
+const fetchTodos = async () => {
+  try {
+    const res = await API.graphql({
+      query: listTodos,
+      authMode: "API_KEY"
+    })
+
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default {
+  name: 'IndexPage',
+  mounted () {
+   fetchTodos()
+  }
+}
+</script>
---
+++ issues/misc/issue9671/src/graphql/subscriptions.js
@@ -0,0 +1,461 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onMutateCustomer = /* GraphQL */ `
+  subscription OnMutateCustomer {
+    onMutateCustomer {
+      id
+      name
+      email
+      Mobile
+      customer_notes
+      admin_notes
+      city {
+        id
+        name
+        country
+        zipCode
+        createdAt
+        updatedAt
+      }
+      cities
+      langs
+      owner
+      createdAt
+      updatedAt
+      customerCityId
+    }
+  }
+`;
+export const onUpdateTodoByID = /* GraphQL */ `
+  subscription OnUpdateTodoByID($id: ID!) {
+    onUpdateTodoByID(id: $id) {
+      id
+      name
+      description
+      file {
+        bucket
+        key
+        region
+        mimeType
+        localUri
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      name
+      description
+      file {
+        bucket
+        key
+        region
+        mimeType
+        localUri
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      name
+      description
+      file {
+        bucket
+        key
+        region
+        mimeType
+        localUri
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      name
+      description
+      file {
+        bucket
+        key
+        region
+        mimeType
+        localUri
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateProject = /* GraphQL */ `
+  subscription OnCreateProject {
+    onCreateProject {
+      id
+      name
+      team {
+        id
+        name
+        createdAt
+        updatedAt
+        teamProjectId
+      }
+      createdAt
+      updatedAt
+      projectTeamId
+    }
+  }
+`;
+export const onUpdateProject = /* GraphQL */ `
+  subscription OnUpdateProject {
+    onUpdateProject {
+      id
+      name
+      team {
+        id
+        name
+        createdAt
+        updatedAt
+        teamProjectId
+      }
+      createdAt
+      updatedAt
+      projectTeamId
+    }
+  }
+`;
+export const onDeleteProject = /* GraphQL */ `
+  subscription OnDeleteProject {
+    onDeleteProject {
+      id
+      name
+      team {
+        id
+        name
+        createdAt
+        updatedAt
+        teamProjectId
+      }
+      createdAt
+      updatedAt
+      projectTeamId
+    }
+  }
+`;
+export const onCreateTeam = /* GraphQL */ `
+  subscription OnCreateTeam {
+    onCreateTeam {
+      id
+      name
+      project {
+        id
+        name
+        createdAt
+        updatedAt
+        projectTeamId
+      }
+      createdAt
+      updatedAt
+      teamProjectId
+    }
+  }
+`;
+export const onUpdateTeam = /* GraphQL */ `
+  subscription OnUpdateTeam {
+    onUpdateTeam {
+      id
+      name
+      project {
+        id
+        name
+        createdAt
+        updatedAt
+        projectTeamId
+      }
+      createdAt
+      updatedAt
+      teamProjectId
+    }
+  }
+`;
+export const onDeleteTeam = /* GraphQL */ `
+  subscription OnDeleteTeam {
+    onDeleteTeam {
+      id
+      name
+      project {
+        id
+        name
+        createdAt
+        updatedAt
+        projectTeamId
+      }
+      createdAt
+      updatedAt
+      teamProjectId
+    }
+  }
+`;
+export const onCreateCustomer = /* GraphQL */ `
+  subscription OnCreateCustomer {
+    onCreateCustomer {
+      id
+      name
+      email
+      Mobile
+      customer_notes
+      admin_notes
+      city {
+        id
+        name
+        country
+        zipCode
+        createdAt
+        updatedAt
+      }
+      cities
+      langs
+      owner
+      createdAt
+      updatedAt
+      customerCityId
+    }
+  }
+`;
+export const onUpdateCustomer = /* GraphQL */ `
+  subscription OnUpdateCustomer {
+    onUpdateCustomer {
+      id
+      name
+      email
+      Mobile
+      customer_notes
+      admin_notes
+      city {
+        id
+        name
+        country
+        zipCode
+        createdAt
+        updatedAt
+      }
+      cities
+      langs
+      owner
+      createdAt
+      updatedAt
+      customerCityId
+    }
+  }
+`;
+export const onDeleteCustomer = /* GraphQL */ `
+  subscription OnDeleteCustomer {
+    onDeleteCustomer {
+      id
+      name
+      email
+      Mobile
+      customer_notes
+      admin_notes
+      city {
+        id
+        name
+        country
+        zipCode
+        createdAt
+        updatedAt
+      }
+      cities
+      langs
+      owner
+      createdAt
+      updatedAt
+      customerCityId
+    }
+  }
+`;
+export const onCreateCity = /* GraphQL */ `
+  subscription OnCreateCity {
+    onCreateCity {
+      id
+      name
+      country
+      zipCode
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateCity = /* GraphQL */ `
+  subscription OnUpdateCity {
+    onUpdateCity {
+      id
+      name
+      country
+      zipCode
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteCity = /* GraphQL */ `
+  subscription OnDeleteCity {
+    onDeleteCity {
+      id
+      name
+      country
+      zipCode
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateJSONData = /* GraphQL */ `
+  subscription OnCreateJSONData {
+    onCreateJSONData {
+      id
+      name
+      data
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateJSONData = /* GraphQL */ `
+  subscription OnUpdateJSONData {
+    onUpdateJSONData {
+      id
+      name
+      data
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteJSONData = /* GraphQL */ `
+  subscription OnDeleteJSONData {
+    onDeleteJSONData {
+      id
+      name
+      data
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateFlight = /* GraphQL */ `
+  subscription OnCreateFlight {
+    onCreateFlight {
+      id
+      pilot
+      airplane {
+        id
+        registration
+        category
+        lastIndex
+        createdAt
+        updatedAt
+      }
+      airplaneId
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateFlight = /* GraphQL */ `
+  subscription OnUpdateFlight {
+    onUpdateFlight {
+      id
+      pilot
+      airplane {
+        id
+        registration
+        category
+        lastIndex
+        createdAt
+        updatedAt
+      }
+      airplaneId
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteFlight = /* GraphQL */ `
+  subscription OnDeleteFlight {
+    onDeleteFlight {
+      id
+      pilot
+      airplane {
+        id
+        registration
+        category
+        lastIndex
+        createdAt
+        updatedAt
+      }
+      airplaneId
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateAircraft = /* GraphQL */ `
+  subscription OnCreateAircraft {
+    onCreateAircraft {
+      id
+      registration
+      category
+      lastIndex
+      specs {
+        passenger
+        fuel
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateAircraft = /* GraphQL */ `
+  subscription OnUpdateAircraft {
+    onUpdateAircraft {
+      id
+      registration
+      category
+      lastIndex
+      specs {
+        passenger
+        fuel
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteAircraft = /* GraphQL */ `
+  subscription OnDeleteAircraft {
+    onDeleteAircraft {
+      id
+      registration
+      category
+      lastIndex
+      specs {
+        passenger
+        fuel
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
